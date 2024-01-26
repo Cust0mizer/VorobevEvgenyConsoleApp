51335b4406dbb59b13fae6e9a9c949175fc1001a
Игорь Некрашевич <misterpekc@gmail.com>
2024-01-26 09:57:41 +0700

    йцуйцуйцу

--- VorobevEvgenyConsoleApp/Program.cs
+++ VorobevEvgenyConsoleApp/Program.cs
@@ -1,15 +1,16 @@
 ﻿namespace VorobevEvgenyConsoleApp {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
-            Console.WriteLine("ASDFASDF");
+            int resultValue = GetSum(12, 12);
+            Console.WriteLine(resultValue);
+        }
+
+        static int GetSum(int firstValue, int secondValue) {
+            return firstValue + secondValue;
         }
     }
 }
+
+//ничего не принимает
+//ничего не возвращает
+//внутри есть переменная которая увеличиваеться на 1
